6b170e0f2637c0bf353832ae060b9855f867a13e
Khumbize <kpvchimsale@gmail.com>
2022-12-09 13:57:06 -0800

    Numbers single digits base 10f from mzimba

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * Description: main - A program that prints all single digit numbers 
+ * You are not allowed to use any variable of type char
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = "0"; num <= "9"; num++)
+		putchar(num);
+	putchar('\n');
+	return (0);
+}
