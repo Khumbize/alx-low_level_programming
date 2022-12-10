3c4bee0e82aa5b57eb03f7b86459c2372b818972
Khumbize <kpvchimsale@gmail.com>
2022-12-10 07:43:55 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * Description: main-program starting point.
+ * Return: 0 for no error and non zero for error
+ */
+
+int main(void)
+{
+	int k;
+	for (k ='0'; k <= '9'; k++)
+		putchar(k);
+	putchar('\n');
+	return (0);
+}
+
