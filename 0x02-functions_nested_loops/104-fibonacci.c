4be5739a92e85605226d7e3ccdac626f25412f8c
Khumbize <kpvchimsale@gmail.com>
2022-12-14 11:09:17 -0800

    updated

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *Description: main - A function that will print 98 fibonacci terms
+ *Return: 0
+ */
+int main(void)
+{
+	int i;
+	int n = 98;
+	int t1 = 1, t2 = 2;
+	int t3 = t1 + t2;
+	printf("Fibonacci Series: %d, %d, ", t1, t2);
+	for (i = 3; i <= n; ++i)
+	{
+		printf("%d, ", t3);
+		t1 = t2;
+		t2 = t3;
+		t3 = t1 + t2;
+	}
+	return (0);
+}
