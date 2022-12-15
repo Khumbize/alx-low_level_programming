d2324d40911980a5d6bb7bc794bf1574d617f57c
Khumbize <kpvchimsale@gmail.com>
2022-12-15 07:13:29 -0800

    multiply

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,13 +2,14 @@
 
 /**
  * Description: mul - a function that multiplies two numbers
+ * @a: number to be multiplied
+ * @b: number to be multiplied
  * Return: 0
  */
-int mul(void)
+int mul(int a, int b)
 {
-	int a;
-	int b;
+	int c;
 
-	return (a * b);
+	c = a * b;
 	return (0);
 }
