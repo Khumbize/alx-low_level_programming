e5412fce551f99289c9673703988dff01397161d
Khumbize <kpvchimsale@gmail.com>
2022-12-15 07:30:01 -0800

    Printing numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * Description: print_numbers - prints some numbers
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
