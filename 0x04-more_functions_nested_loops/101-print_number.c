6d46f42617663e4e2ac69036a75b74bd1b2c01ee
Khumbize <kpvchimsale@gmail.com>
2022-12-15 13:10:56 -0800

    intergers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * Description: print_number - a function that prints integers
+ * @n: the integer
+ * Return: 0
+ */
+void print_number(int n)
+{
+	for (n > 0)
+	scanf("%d", &n)
+	_putchar("%d\n", n);
+	else
+		_putchar("Not");
+		_putchar("an");
+		_putchar("interger");
+	return (0);
+}
