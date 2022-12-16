af4b13a2345ad309711249fbba8380526fc0be21
Khumbize <kpvchimsale@gmail.com>
2022-12-15 22:13:08 -0800

    int

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * Description: print_number - a function that prints integers
@@ -7,16 +8,28 @@
  */
 void print_number(int n)
 {
+	char text[10] = "Enternum";
+	int  i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(text[i]);
+	}
+	scanf("%d", &n);
+	_putchar('\n');
 	if (n > 0)
 	{
-		scanf("%d", &n)
 		_putchar("%d\n", n);
 	}
 	else
 	{
-		_putchar("Not");
-		_putchar("an");
-		_putchar("interger");
+		_putchar("N");
+		_putchar("o");
+		_putchar("t");
+		_putchar(" ");
+		_putchar("i");
+		_putchar("n");
+		_putchar("t");
 	}
 	return (0);
 }
