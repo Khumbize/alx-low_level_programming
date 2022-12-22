76fd21b27c68daf57edd671f101e3cd341cf8685
Khumbize <kpvchimsale@gmail.com>
2022-12-22 12:10:18 -0800

    number 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * Description: print_number - a function that prints and integer.
+ * Return: Always 0.
+ * @n: number
+ */
+void print_number(int n)
+{
+	unsigned int size = n;
+
+	int num = 0;
+
+	int x;
+
+	if (n < 0)
+		size = -n;
+	if (n == 0)
+		num = 1;
+
+	while (size >= 1)
+	{
+		size = size / 10;
+		num++;
+	}
+
+	for (x = 0; x < num; x++)
+	{
+
+		int pow = 1;
+
+		int y;
+
+		int d;
+
+		for (y = 0; y < num - x - 1; y++)
+		{
+			pow = pow * 10;
+		}
+
+		d = ((n / pow) % 10);
+		if (n < 0)
+		{
+			d = -d;
+			if (x == 0)
+				_putchar(45);
+		}
+		_putchar(48 + d);
+	}
+}
