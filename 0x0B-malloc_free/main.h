257410f72986469aa438eb69d259d60f1d8ed795
Khumbize <kpvchimsale@gmail.com>
2023-01-10 13:16:16 -0800

    checking

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,9 +8,9 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
-char **strtow(char *str);
 int getWords(char *s);
 int get_size_of_first(char *s);
 void fill(char *s1, char *s2, int size);
+char **strtow(char *str);
 
 #endif
