35911da99f24a8c1440b8c83f3e81585dd7f3838
Khumbize <kpvchimsale@gmail.com>
2023-01-12 20:39:02 -0800

    redone

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -90,11 +90,11 @@ int check_for_digits(char **av)
  */
 void init(char *str, int l)
 {
-	int i;
+	int p;
 
-	for (i = 0; i < l; i++)
-		str[i] = '0';
-	str[i] = '\0';
+	for (p = 0; p < l; p++)
+		str[p] = '0';
+	str[p] = '\0';
 }
 
 /**
@@ -106,41 +106,41 @@ void init(char *str, int l)
 
 int main(int argc, char *argv[])
 {
-	int l1, l2, ln, ti, i;
+	int length1, length2, length, ko, i;
 	char *a;
 	char *t;
 	char e[] = "Error\n";
 
 	if (argc != 3 || check_for_digits(argv))
 	{
-		for (ti = 0; e[ti]; ti++)
-			_putchar(e[ti]);
+		for (ko = 0; e[ko]; ko++)
+			_putchar(e[ko]);
 		exit(98);
 	}
-	for (l1 = 0; argv[1][l1]; l1++)
+	for (length1 = 0; argv[1][length1]; length1++)
 		;
-	for (l2 = 0; argv[2][l2]; l2++)
+	for (length2 = 0; argv[2][length2]; length2++)
 		;
-	ln = l1 + l2 + 1;
-	a = malloc(ln * sizeof(char));
+	length = length1 + length2 + 1;
+	a = malloc(length * sizeof(char));
 	if (a == NULL)
 	{
-		for (ti = 0; e[ti]; ti++)
-			_putchar(e[ti]);
+		for (ko = 0; e[ko]; ko++)
+			_putchar(e[ko]);
 		exit(98);
 	}
-	init(a, ln - 1);
-	for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
+	init(a, length - 1);
+	for (ko = length - 1, i = 0; ko >= 0; ko--, i++)
 	{
-		t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
+		t = mul(argv[2][ko], argv[1], length1 - 1, a, (length - 2) - i);
 		if (t == NULL)
 		{
-			for (ti = 0; e[ti]; ti++)
-				_putchar(e[ti]);
+			for (ko = 0; e[ko]; ko++)
+				_putchar(e[ko]);
 			free(a);
 			exit(98);
 		}
 	}
-	_print(a, ln - 1);
+	_print(a, length - 1);
 	return (0);
 }
