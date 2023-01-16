860ef9f94817dd3fa1795a9483b9d8bbd28a8011
Khumbize <kpvchimsale@gmail.com>
2023-01-16 13:23:22 -0800

    created a dog structure, initialized and printed

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - a structure  for a dog.
+ * @name: dog name.
+ * @age: dog age.
+ * @owner: dog owner.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+int _strlen(char *s);
+void free_dog(dog_t *d);
+
+#endif
