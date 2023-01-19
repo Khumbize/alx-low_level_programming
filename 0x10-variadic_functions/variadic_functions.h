ad6b7c8939b60ff423c7ff162baebf6a389c62d8
Khumbize <kpvchimsale@gmail.com>
2023-01-19 13:45:18 -0800

    variadic function are so much fun

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,11 @@
+#ifndef VARIADIC_FUNCTIONS
+#define VARIADIC_FUNCTIONS
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+int isAval(char c);
+
+#endif
