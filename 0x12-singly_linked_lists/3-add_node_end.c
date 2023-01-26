1965cfe990fe1a39179979ced6f8d579c4ac18ee
Khumbize <kpvchimsale@gmail.com>
2023-01-26 14:35:25 -0800

    3rd is perseverance

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,24 +1,6 @@
 #include "lists.h"
 
 /**
- * _strlen - returns length of string
- * @s: string
- * Return: length of string
- */
-
-int _strlen(const char *s)
-{
-	int len;
-
-	len = 0;
-	while (s[len] != '\0')
-	{
-		len++;
-	}
-	return (len);
-}
-
-/**
  * add_node_end - adds a new node at the end of  list_t list
  * @head: head of linked list
  * @str: string to be used as data for node
